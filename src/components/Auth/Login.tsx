abe7bb2e43986cb1575a057f0e1a121233dba023
artrsyf <rom.fadeev2017@yandex.ru>
2025-05-10 18:06:17 +0300

    Add styles for auth flow

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,48 +1,79 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react-lite";
 import { authStore } from "../../store/AuthStore";
+import { Link, useNavigate } from "react-router-dom";
+import "./styles/Auth.css";
 
 const Login: React.FC = observer(() => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
       await authStore.login(username, password);
-      if (!authStore.isAuthenticated) {
-        setError("Login failed");
-      }
+      
+      navigate('/');
     } catch (err) {
       console.error("Login error:", err);
-      setError("Login failed");
+      
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError(String(err));
+      }
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username:</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
+    <div className="content_wrapper">
+      <div className="login_window">
+        <form onSubmit={handleSubmit} className="form_wrapper">
+          <span className="login_header">Login</span>
+
+          <div className="form_text_input_wrapper">
+            <input
+              type="text"
+              className="form_text_input"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form_text_input_wrapper">
+            <input
+              type="password"
+              className="form_text_input"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          {error && <div className="error_message">{error}</div>}
+
+          <div className="login_button_wrapper">
+            <button type="submit" className="login_button">
+              SIGN IN
+            </button>
+          </div>
+
+          <div className="have_no_acc_wrapper">
+            <span className="have_no_acc_text">
+              Don't have an account?{" "}
+              <Link to="/signup" className="non_ref signup_link">
+                Sign Up
+              </Link>
+            </span>
+          </div>
+        </form>
       </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">Login</button>
-      {error && <p>{error}</p>}
-    </form>
+    </div>
   );
 });
 
--- src/components/Auth/Signup.tsx
+++ src/components/Auth/Signup.tsx
@@ -1,49 +1,79 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react-lite";
 import { authStore } from "../../store/AuthStore";
+import { Link, useNavigate  } from "react-router-dom";
+import "./styles/Auth.css";
 
 const Register: React.FC = observer(() => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      // Вызываем метод регистрации
       await authStore.register(username, password);
-      if (!authStore.isAuthenticated) {
-        setError("Registration failed");
-      }
+      
+      navigate('/');
     } catch (err) {
       console.error("Registration error:", err);
-      setError("Registration failed");
+      
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError(String(err));
+      }
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username:</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
+    <div className="content_wrapper">
+      <div className="login_window">
+        <form onSubmit={handleSubmit} className="form_wrapper">
+          {error && <div className="error_message">{error}</div>}
+          
+          <span className="login_header">Sign Up</span>
+
+          <div className="form_text_input_wrapper">
+            <input
+              type="text"
+              className="form_text_input"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form_text_input_wrapper">
+            <input
+              type="password"
+              className="form_text_input"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="login_button_wrapper">
+            <button type="submit" className="login_button">
+              CREATE ACCOUNT
+            </button>
+          </div>
+
+          <div className="have_no_acc_wrapper">
+            <span className="have_no_acc_text">
+              Already have an account?{" "}
+              <Link to="/login" className="non_ref signup_link">
+                Sign In
+              </Link>
+            </span>
+          </div>
+        </form>
       </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">Register</button>
-      {error && <p>{error}</p>}
-    </form>
+    </div>
   );
 });
 
